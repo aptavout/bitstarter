b09848537ea7ed43b3ba0891fac9972b984327a3
ettis <apt.avout@gmail.com>
2013-07-04 05:41:11 +0000

    try fs.filereadsync

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync("index.html");
+  var output = buf.toString(buffer);
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
