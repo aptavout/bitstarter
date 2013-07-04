5da19e9168fff86a89b3ba7a6de666d0f5b1c598
ettis <apt.avout@gmail.com>
2013-07-04 05:43:52 +0000

    object invoking method is param

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync("index.html");
-  var output = buffer.toString(buffer);
+  var output = buffer.toString();
   response.send(output);
 });
 
