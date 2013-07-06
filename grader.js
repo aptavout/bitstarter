1fbab0508dc4085b04fb2a65628ae4d4216481f9
ettis <apt.avout@gmail.com>
2013-07-06 06:14:27 +0000

    agh, how to block for network i/o?

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -20,10 +21,11 @@ var cheerioHtmlFile = function(htmlfile) {
 };
 
 var loadChecks = function(checksfile) {
-	return JSON.parse(fs.readFileSync(checksfile);
+	return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
+	console.error('htmlfile: ' + htmlfile);
 	$ = cheerioHtmlFile(htmlfile);
 	var checks = loadChecks(checksfile).sort();
 	var out = {};
@@ -34,16 +36,46 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 	return out;
 };
 
+var clone = function(fn) {
+	return fn.bind({});
+};
+
+var httpresponse = function() {
+	var resp = function(result, response) {
+		fs.writeFileSync("get.html", result);
+	};
+	return resp;	
+};
+
+var fetchURL = function(httpurl, checksfile) {
+	var urlstr = httpurl.toString();
+	console.error('httpurl: ' + urlstr);
+	var outstr = httpresponse(); 
+	var res = rest.get(urlstr).on('complete', outstr);
+	if (res) 
+		return outstr;	
+	else
+		console.error("FAIL TO RESTLER");
+};
+
 if (require.main == module) {
 	program
-		.option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-		.option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		//.option('-u, --url <url>', 'Path to url', clone(fetchURL))
+		.option('-u, --url <url>', 'Path to url')
 		.parse(process.argv);
 
-	var checkJson = checkHtmlFile(program.file, program.checks);
+	if (program.url) {
+		var checkJson = fetchURL(program.url, program.checks);
+		var checkJson = checkHtmlFile("get.html", program.checks);
+	}
+	else if (program.file) {
+		var checkJson = checkHtmlFile(program.file, program.checks);
+	}
 	var outJson = JSON.stringify(checkJson, null, 4);
 	console.log(outJson);
 } else {
-	exports.checkHtmlFile = checkHtmlFile;
+//	exports.checkHtmlFile = checkHtmlFile;
 }
 
