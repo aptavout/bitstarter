d34bbe68ab77a3f89c14701f21a0ffb1a743267b
ettis <apt.avout@gmail.com>
2013-07-07 03:20:12 +0000

    do stuff immediately after fs file sync write

--- grader.js
+++ grader.js
@@ -40,9 +40,13 @@ var clone = function(fn) {
 	return fn.bind({});
 };
 
-var httpresponse = function() {
+var httpresponse = function(checksfile) {
 	var resp = function(result, response) {
 		fs.writeFileSync("get.html", result);
+		var innercheck = checkHtmlFile("get.html", checksfile);
+		//console.error('innercheck: ' + innercheck); // [Object]
+		var jsonstr = JSON.stringify(innercheck, null, 4);
+		console.log(jsonstr); // this is your only chance to print it
 	};
 	return resp;	
 };
@@ -50,12 +54,18 @@ var httpresponse = function() {
 var fetchURL = function(httpurl, checksfile) {
 	var urlstr = httpurl.toString();
 	console.error('httpurl: ' + urlstr);
-	var outstr = httpresponse(); 
+	var outstr = httpresponse(checksfile); 
 	var res = rest.get(urlstr).on('complete', outstr);
 	if (res) 
 		return outstr;	
 	else
 		console.error("FAIL TO RESTLER");
+};
+
+// this function will be evaluated immediately in the main 'loop,' so 
+// this idea doesn't work
+var lateboundreturn = function(fn) {
+	return fn;
 };
 
 if (require.main == module) {
@@ -68,13 +78,20 @@ if (require.main == module) {
 
 	if (program.url) {
 		var checkJson = fetchURL(program.url, program.checks);
-		var checkJson = checkHtmlFile("get.html", program.checks);
+		/* blocking network i/o: must do all work after file sync
+			inside of Restler HTTP GET response */
+		//var checkJson = checkHtmlFile("get.html", program.checks);
+		//var wrap = lateboundreturn(checkJson);
+		//var outJson = JSON.stringify(wrap, null, 4); // ...
+		//console.error('outJson: ' + outJson);
 	}
 	else if (program.file) {
 		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
 	}
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	//var outJson = JSON.stringify(checkJson, null, 4);
+	//console.log(outJson);
 } else {
 //	exports.checkHtmlFile = checkHtmlFile;
 }
